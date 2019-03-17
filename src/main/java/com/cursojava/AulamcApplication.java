8172b1a8f5100b20387306dbdfd1a41f4e59bd1e
tathianelima <tathiane.lima@hotmail.com>
2019-03-17 15:45:08 -0300

    Projeto criado

---
+++ src/main/java/com/cursojava/AulamcApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamcApplication.class, args);
+	}
+
+}
