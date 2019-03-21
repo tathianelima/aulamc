5cf95ec02f981baf54fcec969ab6620f424bd036
tathianelima <tathiane.lima@hotmail.com>
2019-03-21 18:43:55 -0300

    Criando operação de Instanciação

--- src/main/java/com/cursojava/AulamcApplication.java
+++ src/main/java/com/cursojava/AulamcApplication.java
@@ -1,13 +1,32 @@
 package com.cursojava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulamcApplication {
+import com.cursojava.domain.Categoria;
+import com.cursojava.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AulamcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulamcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
