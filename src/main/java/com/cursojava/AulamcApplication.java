ffffc29b8c8069efdbacc7089246ba2c3589f60e
tathianelima <tathiane.lima@hotmail.com>
2019-04-03 13:56:55 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/cursojava/AulamcApplication.java
+++ src/main/java/com/cursojava/AulamcApplication.java
@@ -13,6 +13,7 @@ import com.cursojava.domain.Cidade;
 import com.cursojava.domain.Cliente;
 import com.cursojava.domain.Endereco;
 import com.cursojava.domain.Estado;
+import com.cursojava.domain.ItemPedido;
 import com.cursojava.domain.Pagamento;
 import com.cursojava.domain.PagamentoComBoleto;
 import com.cursojava.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.cursojava.repositories.CidadeRepository;
 import com.cursojava.repositories.ClienteRepository;
 import com.cursojava.repositories.EnderecoRepository;
 import com.cursojava.repositories.EstadoRepository;
+import com.cursojava.repositories.ItemPedidoRepository;
 import com.cursojava.repositories.PagamentoRepository;
 import com.cursojava.repositories.PedidoRepository;
 import com.cursojava.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class AulamcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 
 	
@@ -113,6 +117,19 @@ public class AulamcApplication implements CommandLineRunner{
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 		
 	}
