1799ca9aaf4a28e7fd8d7bcc8c960652e15faa04
tathianelima <tathiane.lima@hotmail.com>
2019-05-16 21:27:38 -0300

    Listando todas as Categorias com DTO

---
+++ src/main/java/com/cursojava/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+package com.cursojava.dto;
+
+import java.io.Serializable;
+
+import com.cursojava.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	CategoriaDTO(){
+	}
+	
+	//instancia um objeto DTO a partir de um objeto categoria
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/com/cursojava/services/CategoriaService.java
+++ src/main/java/com/cursojava/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.cursojava.services;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,9 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
