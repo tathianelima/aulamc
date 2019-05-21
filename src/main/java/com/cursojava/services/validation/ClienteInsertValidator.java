aebd7527614925981afec38256d378d08de94deb
tathianelima <tathiane.lima@hotmail.com>
2019-05-20 22:44:28 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/cursojava/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursojava/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursojava.domain.Cliente;
 import com.cursojava.domain.enums.TipoCliente;
 import com.cursojava.dto.ClienteNewDTO;
+import com.cursojava.repositories.ClienteRepository;
 import com.cursojava.resourses.exceptions.FieldMessage;
 import com.cursojava.services.validation.utils.BR;
 
 
 // Fonte: https://gist.github.com/adrianoluis/5043397d378ae506d87366abb0ab4e30
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -24,6 +31,7 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		
 		List<FieldMessage> list = new ArrayList<>();
+		// inclua os testes aqui, inserindo erros na lista
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
@@ -33,7 +41,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
 		
-		// inclua os testes aqui, inserindo erros na lista
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
